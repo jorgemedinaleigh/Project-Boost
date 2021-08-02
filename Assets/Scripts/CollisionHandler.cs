c22cd8627d1b8e712eb7db1ee2963593c6fdeebb
Jorge Medina <jorgemedinaleigh@protonmail.com>
2021-08-02 17:25:01 -0400

    Added Scene Manager and Collision Handler

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly");
+                break;
+            case "Finish":
+                Debug.Log("This is the Finish");
+                LoadNextLevel();
+                break;
+            case "Fuel":
+                Debug.Log("Fuel picked");
+                break;
+            default:
+                Debug.Log("You Explode!");
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
